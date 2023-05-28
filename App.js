1c2351daec5b9f92919b41ae64c388ede953684a
Elton Santos <eltonaxl@hotmail.com>
2023-05-28 02:46:45 -0300

    Create todo

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  TouchableOpacity, 
+  TextInput
+ } from 'react-native'
+
+import { FontAwesome } from '@expo/vector-icons'
 
 export default function App(){
-  const [nome, setNome] = useState('Elton')
+  const [tarefa, setTarefa] = useState('');
 
-  function handleMudaNome(){
-    setNome('Sujeito Programador')
+
+  function handleAdd(){
+    alert(tarefa)
   }
 
   return(
     <View style={styles.container}>
-      <Text style={styles.title}>{nome}</Text>
+      <Text style={styles.title}>Tarefas</Text>
 
-      <TouchableOpacity style={styles.button} onPress={handleMudaNome}>
-        <Text style={styles.buttonText}>Mudar nome</Text>
-      </TouchableOpacity>
+      <View style={styles.containerInput}>
+        <TextInput
+          placeholder="Digite sua tarefa..."
+          style={styles.input}
+          value={tarefa}
+          onChangeText={ (text) => setTarefa(text) }
+        />
+
+        <TouchableOpacity style={styles.buttonAdd} onPress={handleAdd}>
+          <FontAwesome name="plus" size={20} color="#FFF" />
+        </TouchableOpacity>
+      </View>
+
     </View>
   )
 }
@@ -22,24 +41,39 @@ export default function App(){
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor: '#f1f1f1',
+    backgroundColor: '#22272e',
     paddingTop: 28,
   },
   title:{
-    fontSize: 32,
-    color: '#121212',
     fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  button:{
-    backgroundColor: 'blue',
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 35
-  },
-  buttonText:{
+    fontSize: 24,
     color: '#FFF',
-    fontWeight: 'bold'
+    marginTop: '5%',
+    paddingStart: '5%',
+    marginBottom: 12,
+  },
+  containerInput:{
+    flexDirection: 'row',
+    width: '100%',
+    height: 44,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 22,
+  },
+  input:{
+    width: '75%',
+    backgroundColor: '#FBFBFB',
+    height: 44,
+    borderRadius: 4,
+    paddingHorizontal: 8,
+  },
+  buttonAdd:{
+    width: '15%',
+    height: 44,
+    backgroundColor: '#73f7ff',
+    marginLeft: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4
   }
 })
